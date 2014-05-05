9f2ed6d89d359a718cc781beba2e6e1ddde72efb
Vincent Hanquez <vincent@snarc.org>
2014-05-05 08:46:49 +0100

    update the benchmark a bit

--- tests/Bench.hs
+++ tests/Bench.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE BangPatterns #-}
 module Main (main) where
 
 import Criterion.Main
@@ -13,50 +14,63 @@ import qualified System.Locale as T
 
 timeToTuple :: T.UTCTime -> (Int, Int, Int, Int, Int, Int)
 timeToTuple utcTime = (fromIntegral y, m, d, h, mi, sec)
- where (y,m,d) = T.toGregorian (T.utctDay utcTime)
-       daytime    = floor $ T.utctDayTime utcTime
-       (dt, sec)= daytime `divMod` 60
-       (h , mi) = dt `divMod` 60
-       --(DateTimeP (Date y m d) (TimeOfDayP (TimeOfDay h mi sec) _)) = localTimeToGlobal localtime
+ where (!y,!m,!d)  = T.toGregorian (T.utctDay utcTime)
+       !daytime    = floor $ T.utctDayTime utcTime
+       (!dt, !sec) = daytime `divMod` 60
+       (!h , !mi)  = dt `divMod` 60
+
+timeToTupleDate :: T.UTCTime -> (Int, Int, Int)
+timeToTupleDate utcTime = (fromIntegral y, m, d)
+  where (!y,!m,!d)  = T.toGregorian (T.utctDay utcTime)
 
 elapsedToPosixTime :: Elapsed -> T.POSIXTime
 elapsedToPosixTime (Elapsed (Seconds s)) = fromIntegral s
 
 timePosixDict :: [ (Elapsed, T.POSIXTime) ]
 timePosixDict =
-    [ (Elapsed 0, 0)
-    , (Elapsed 1000000, 1000000)
-    , (Elapsed 9000099, 9000099)
-    , (Elapsed 1398232846, 1398232846) -- currentish time (at the time of writing)
-    , (Elapsed 5134000099, 5134000099)
-    , (Elapsed 10000000000000, 10000000000000) -- year 318857 ..
+    [-- (Elapsed 0, 0)
+    --, (Elapsed 1000000, 1000000)
+    --, (Elapsed 9000099, 9000099)
+    {-,-} (Elapsed 1398232846, 1398232846) -- currentish time (at the time of writing)
+    --, (Elapsed 5134000099, 5134000099)
+    --, (Elapsed 10000000000000, 10000000000000) -- year 318857 ..
     ]
 
 dateDict :: [ (Int, Int, Int, Int, Int, Int) ]
 dateDict =
-    [ (1970, 1, 1, 1, 1, 1)
-    , (2014, 5, 5, 5, 5, 5)
-    , (2114, 11, 5, 5, 5, 5)
+    [{- (1970, 1, 1, 1, 1, 1)
+    , -}(2014, 5, 5, 5, 5, 5)
+    --, (2114, 11, 5, 5, 5, 5)
     ]
 
--- T.posixSecondsToUTCTime timePosix
-
+main :: IO ()
 main = defaultMain
-    [ bgroup "highlevel" $ concatMap toHighLevel timePosixDict
-    , bgroup "to-calendar"  $ concatMap toCalendar timePosixDict
-    , bgroup "to-posix" $ concatMap toPosix dateDict
+    [ bgroup "highlevel"   $ concatMap toHighLevel timePosixDict
+    , bgroup "to-dateTime" $ concatMap toCalendar timePosixDict
+    , bgroup "to-date"     $ concatMap toCalendarDate timePosixDict
+    , bgroup "utc-to-date" $ concatMap toCalendarUTC timePosixDict
+    , bgroup "to-posix"    $ concatMap toPosix dateDict
     ]
   where toHighLevel (posixHourglass, posixTime) =
-            [ bench (show $ posixHourglass) $ nf timeGetDateTimeOfDay posixHourglass
-            , bench (show $ posixTime) $ nf T.posixSecondsToUTCTime posixTime
+            [ bench (showH posixHourglass) $ nf timeGetDateTimeOfDay posixHourglass
+            , bench (showT posixTime) $ nf T.posixSecondsToUTCTime posixTime
             ]
         toCalendar (posixHourglass, posixTime) =
-            [ bench (show $ posixHourglass) $ nf timeGetDateTimeOfDay posixHourglass
-            , bench (show $ posixTime) $ nf (timeToTuple . T.posixSecondsToUTCTime) posixTime
+            [ bench (showH posixHourglass) $ nf timeGetDateTimeOfDay posixHourglass
+            , bench (showT posixTime) $ nf (timeToTuple . T.posixSecondsToUTCTime) posixTime
             ]
+        toCalendarDate (posixHourglass, posixTime) =
+            [ bench (showH posixHourglass) $ nf timeGetDate posixHourglass
+            , bench (showT posixTime) $ nf (timeToTupleDate . T.posixSecondsToUTCTime) posixTime
+            ]
+        toCalendarUTC (posixHourglass, posixTime) = 
+            [ bench (showH posixHourglass) $ nf timeGetDateTimeOfDay posixHourglass
+            , bench (showT utcTime) $ nf timeToTuple utcTime
+            ]
+          where !utcTime = T.posixSecondsToUTCTime posixTime
         toPosix v =
-            [ bench (n v) $ nf hourglass v
-            , bench (n v) $ nf time v
+            [ bench ("hourglass/" ++ n v) $ nf hourglass v
+            , bench ("time/" ++ n v) $ nf time v
             ]
           where n (y,m,d,h,mi,s) = (intercalate "-" $ map show [y,m,d]) ++ " " ++ (intercalate ":" $ map show [h,mi,s])
                 hourglass (y,m,d,h,mi,s) = timeGetElapsed $ DateTime (Date y (toEnum (m-1)) d) (TimeOfDay h mi s 0)
@@ -64,4 +78,7 @@ main = defaultMain
                                                diffTime = T.secondsToDiffTime $ fromIntegral (h * 3600 + mi * 60 + s)
                                             in T.utcTimeToPOSIXSeconds (T.UTCTime day diffTime)
 
-            --bench (show $ fromIntegral posixHourglass) $ nf (
+        showH :: Show a => a -> String
+        showH a = "hourglass/" ++ show a
+        showT :: Show a => a -> String
+        showT a = "time/" ++ show a
