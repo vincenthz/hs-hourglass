9bc3a8213642cbeead8f7b4114b0888fe361d9aa
Nicolas DI PRIMA <nicolas@di-prima.fr>
2016-10-05 18:47:47 +0100

    remove the instance of Time of LocalTime

--- Data/Hourglass/Local.hs
+++ Data/Hourglass/Local.hs
@@ -58,8 +58,6 @@ instance Time t => Timeable (LocalTime t) where
     timeGetElapsedP    = timeGetElapsedP    . localTimeToGlobal
     timeGetElapsed     = timeGetElapsed     . localTimeToGlobal
     timeGetNanoSeconds = timeGetNanoSeconds . localTimeToGlobal
-instance Time t => Time (LocalTime t) where
-    timeFromElapsedP = localTimeFromGlobal . timeFromElapsedP
 
 -- | Create a local time type from a timezone and a time type.
 --
